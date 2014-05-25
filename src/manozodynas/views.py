38f777007164bda33493260ea29aa032b89853c3
Bandiz <linas.jakseboga@gmail.com>
2014-05-25 14:37:34 +0300

    Added word view_page view

--- src/manozodynas/views.py
+++ src/manozodynas/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from .forms import LoginForm
 from django.contrib.auth import login
+from .models import Word
 
 def index_view(request):
     return render(request, 'manozodynas/index.html', {})
@@ -19,3 +20,7 @@ def login_view(request):
         form = LoginForm()
     #import ipdb; ipdb.set_trace()
     return render(request, 'manozodynas/login.html', {'form':form})
+
+
+def word_view(request):
+    return render(request, 'manozodynas/words.html', {'words':Word.objects.all()})
